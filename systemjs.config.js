3dafbb71d94751e3756a5f4e6746338884566d56
Vinitha Kontham <vinitha.kontham@adp.com>
2018-10-28 12:29:19 -0400

    app runs

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,23 +1,23 @@
-{function(global) {
+(function(global) {
     System.config({
         paths: {
-            'npm': 'node_modules/'
+            'npm:': 'node_modules/'
         },
         map: {
             app: 'app',
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-            '@angular/common': 'npm:@angular/commom/bundles/common.umd.js',
-            '@angular/complier': 'npm:@angular/compiler/bundles/compiler.umd.js',
+            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
             '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-            '@amgular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-            
-            'rxjs': 'npm-rxjs',
-            'angular-in-memory-web-api': 'npm;angualr-in-memory-web-api'
-        },
+            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
+            'rxjs': 'npm:rxjs',
+            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api'
+        },
+        
         packages: {
             app: {
                 main: './main.js',
@@ -32,5 +32,4 @@
             }
         }
     });
-}
-)(this);
+})(this);
